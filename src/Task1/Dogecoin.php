ed834f1ed121a4cfe7a0d1a1d7af2513d1dbd4a5
Mogave <lesogor.kirill@gmail.com>
2018-05-09 16:59:05 +0300

    Commiting strait in dev? That's bad. Won't do that again for sure

---
+++ src/Task1/Dogecoin.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+
+namespace Cryptocurrency\Task1;
+
+
+final class Dogecoin extends AbstractCurrency
+{
+    protected static $name = 'Dogecoin';
+    protected static $logoUrl = 'https://s2.coinmarketcap.com/static/img/coins/32x32/74.png';
+}
