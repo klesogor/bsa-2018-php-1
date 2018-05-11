3d8b957a3ff649a9cad8903ff8f8017309ee88e0
Mogave <lesogor.kirill@gmail.com>
2018-05-11 17:38:19 +0300

    Trying to move it to staging server since you need VT-x to run homestead. Hate OpenServer

--- src/Task3/MarketHtmlPresenter.php
+++ src/Task3/MarketHtmlPresenter.php
@@ -3,11 +3,114 @@
 namespace Cryptocurrency\Task3;
 
 use Cryptocurrency\Task1\CoinMarket;
+use Cryptocurrency\Task1\Currency;
 
 class MarketHtmlPresenter
 {
     public function present(CoinMarket $market): string
     {
-        // @todo
+        $result = '';
+        $result.=$this->renderHead();
+        $result.='<body>';
+        $result.=$this->renderHeader();
+        $result.=$this->renderBody($market);
+        $result.='</body>';
+        return $result;
     }
+
+    public function renderException():string
+    {
+        return 'Whoops, something went wrong. We will try to fix this ASAP, but you can motivate us to work harder with some coins ;)';
+    }
+
+    public function renderHead()
+    {
+       return' <head><meta charset="UTF-8"><meta name="viewport"
+           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="/src/Task3/assets/main.css">
+            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" 
+            integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+            <title>Built-in Web Server</title>
+            </head>';
+    }
+    public function renderHeader():string
+    {
+       return ' <div class = "container-fluid header">
+                <i class="fas fa-chart-line"></i>
+                <span>CryptoTrade</span>
+                </div>';
+    }
+    public function renderBody(CoinMarket $market):string
+    {
+        $result = '<div class="container-fluid" id="body">
+                <div class = "content-header">
+                    What can I do here?
+                </div>
+                <div class = "row">
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+                        <div class="info-column">
+                            <div class = "column-title">
+                                <i class="fab fa-bitcoin"></i>
+                                BUY
+                            </div>
+                            <span class="column-description">
+                                Convert your regular money to super modern extremly secure 
+                                and profitable cryptocurrencies.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+                        <div class = "column-title">
+                            <i class="fas fa-suitcase"></i>
+                            SELL
+                        </div>
+                        <span class="column-description">
+                            Running short on regular money? <br>
+                            1) Sell some cryptocurrencies <br>
+                            2) ????? <br>
+                            3) PROFIT!
+                        </span>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-xs-12 col-sm-12">
+                        <div class = "column-title">
+                            <i class="fas fa-exchange-alt"></i>
+                            CONVERT
+                        </div>
+                        <span class="column-description">
+                            Convert your regular money to super modern extremly secure 
+                            and profitable cryptocurrencies.
+                        </span>
+                    </div>
+                </div>
+                <!-- Currencies block !-->
+                <div class = "currencies">
+                        <div class = "content-header">
+                            Currencies
+                        </div>';
+        foreach($market->getCurrencies() as $currency) {
+            $result.= $this->renderCurrency($currency);
+        }
+        $result.='</div></div>';
+        return $result;
+    }
+
+    protected function renderCurrency(Currency $currency):string
+    {
+        return "
+        <div class='currency-container'>
+            <img src='{$currency->getLogoUrl()}' alt='currency logo'>
+            <span>
+                {$currency->getName()}
+            </span>
+            <span style = 'float:right'>
+                Daily price:{$currency->getDailyPrice()}
+            </span>
+         </div>
+        ";
+    }
+
 }
